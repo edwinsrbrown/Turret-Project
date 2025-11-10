d613355208b885c37ff8e194c95f8f5896179440
cchang04 <cchang04@terpmail.umd.edu>
2025-11-10 17:54:52 -0500

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -1,48 +1,47 @@
+"""
+Connor Chang
+ENME441 - Lab 6
+"""
 
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 
+serialPin, latchPin, clockPin = 23, 24, 25
+
 class Shifter:
-  GPIO.setmode(GPIO.BCM)
-  #Def for various pins
-  def __init__(self, serialPin, latchPin, clockPin):
+  def __init__(self, serialPin, latchPin, clockPin): #initialize the Shifter class with its attributes
     self.serialPin = serialPin
     self.latchPin = latchPin
     self.clockPin = clockPin
+    
+    GPIO.setup(serialPin, GPIO.OUT)
+    GPIO.setup(latchPin, GPIO.OUT, initial=0)
+    GPIO.setup(clockPin, GPIO.OUT, initial=0)
 
-    GPIO.setup(self.serialPin, GPIO.OUT)
-    GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
-    GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
-  #ping method
-  def ping(self, pin):
-    GPIO.output(pin, 1)
+  def __ping(self, p):
+    GPIO.output(p, 1)
     time.sleep(0)
-    GPIO.output(pin, 0)
-  #shift method
+    GPIO.output(p, 0)
+
   def shiftByte(self, b):
     for i in range(8):
       GPIO.output(self.serialPin, b & (1 << i))
-      self.ping(self.clockPin)
-    self.ping(self.latchPin)
+      """
+      creates 8 separate bytes, each with the binary number 1 shifted to the left by 1 index each run through the for loop
+      for each byte, checks if both the b pattern and the shifted byte has a 1 at each index
+      if the both have a 1, GPIO pin is TRUE (turns LED on), if not, pin is FALSE (keeps LED off)
+      """
+      self.__ping(self.clockPin)
+    self.__ping(self.latchPin)
 
-#only run if file is executed directly
-if __name__=="__main__":
-  #use class
-  shift = Shifter(serialPin=23, latchPin=24, clockPin=25)
-
+if __name__ == "__main__": #only run the following code when shifter.py is directly run (used to test code)
+  shifter = Shifter(serialPin, latchPin, clockPin)
+ 
   try:
-    while 1:
-      for i in range(2**8):
-        shift.shiftByte(i)
-        time.sleep(0.5)
+    while 1: 
+      shifter.shiftByte(0b01010101)
+  
   except KeyboardInterrupt:
     GPIO.cleanup()
-  
-
-
-
-
-
-
