975b5889783e0d670fec546ca5dddee8a809186a
edwinsrbrown <ebrown55@terpmail.umd.edu>
2025-11-11 15:42:41 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -80,7 +80,7 @@ class Stepper:
 
     # Move relative angle from current position:
     def __rotate(self, delta):
-        self.lock.acquire()                 # wait until the lock is available
+        # self.lock.acquire()                 # wait until the lock is available
         numSteps = int(Stepper.steps_per_degree * abs(delta))    # find the right # of steps
         dir = self.__sgn(delta)        # find the direction (+/-1)
         for s in range(numSteps):      # take the steps
