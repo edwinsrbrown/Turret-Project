f8c5ed052d981d36d469406762e108c73ad168d8
Nishan-567 <nsandhu@terpmail.umd.edu>
2025-11-10 19:42:12 -0500

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -1,47 +1,48 @@
-"""
-Connor Chang
-ENME441 - Lab 6
-"""
+# Shift register class
 
-import RPi.GPIO as GPIO
-import time
+from RPi import GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-serialPin, latchPin, clockPin = 23, 24, 25
+class Shifter():
 
-class Shifter:
-  def __init__(self, serialPin, latchPin, clockPin): #initialize the Shifter class with its attributes
-    self.serialPin = serialPin
-    self.latchPin = latchPin
-    self.clockPin = clockPin
-    
-    GPIO.setup(serialPin, GPIO.OUT)
-    GPIO.setup(latchPin, GPIO.OUT, initial=0)
-    GPIO.setup(clockPin, GPIO.OUT, initial=0)
+    def __init__(self, data, clock, latch):
+        self.dataPin = data
+        self.latchPin = latch
+        self.clockPin = clock
+        GPIO.setup(self.dataPin, GPIO.OUT)
+        GPIO.setup(self.latchPin, GPIO.OUT)
+        GPIO.setup(self.clockPin, GPIO.OUT)
 
-  def __ping(self, p):
-    GPIO.output(p, 1)
-    time.sleep(0)
-    GPIO.output(p, 0)
+    def ping(self, p):  # ping the clock or latch pin
+        GPIO.output(p,1)
+        sleep(0)
+        GPIO.output(p,0)
 
-  def shiftByte(self, b):
-    for i in range(8):
-      GPIO.output(self.serialPin, b & (1 << i))
-      """
-      creates 8 separate bytes, each with the binary number 1 shifted to the left by 1 index each run through the for loop
-      for each byte, checks if both the b pattern and the shifted byte has a 1 at each index
-      if the both have a 1, GPIO pin is TRUE (turns LED on), if not, pin is FALSE (keeps LED off)
-      """
-      self.__ping(self.clockPin)
-    self.__ping(self.latchPin)
+    # Shift all bits in an arbitrary-length word, allowing
+    # multiple 8-bit shift registers to be chained (with overflow
+    # of SR_n tied to input of SR_n+1):
+    def shiftWord(self, dataword, num_bits):
+        for i in range((num_bits+1) % 8):  # Load bits short of a byte with 0
+            # self.dataPin.value(0)  # MicroPython for ESP32
+            GPIO.output(self.dataPin, 0) 
+            self.ping(self.clockPin)
+        for i in range(num_bits):          # Send the word
+            # self.dataPin.value(dataword & (1<<i))  # MicroPython for ESP32
+            GPIO.output(self.dataPin, dataword & (1<<i))
+            self.ping(self.clockPin)
+        self.ping(self.latchPin)
 
-if __name__ == "__main__": #only run the following code when shifter.py is directly run (used to test code)
-  shifter = Shifter(serialPin, latchPin, clockPin)
- 
-  try:
-    while 1: 
-      shifter.shiftByte(0b01010101)
-  
-  except KeyboardInterrupt:
-    GPIO.cleanup()
+    # Shift all bits in a single byte:
+    def shiftByte(self, databyte):
+        self.shiftWord(databyte, 8)
+
+
+# Example:
+#
+# from time import sleep
+# s = Shifter(data=16,clock=20,latch=21)   # convenient Pi pins
+# for i in range(256):
+#     s.shiftByte(i)
+#     sleep(0.1)
