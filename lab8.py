aab11318194a9d84d1f727f2047a31d186d12904
edwinsrbrown <ebrown55@terpmail.umd.edu>
2025-11-11 22:45:46 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -131,7 +131,7 @@ if __name__ == '__main__':
     # If separate multiprocessing.lock objects are used, the second motor
     # will run in parallel with the first motor:
     
-    m2.go Angle(180)
+    m2.goAngle(180)
     m2.rotate(-45)
     m2.rotate(45)
     m2.rotate(-90)
