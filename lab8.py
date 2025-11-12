f17173980df00c585005aee439151fdc295c3016
edwinsrbrown <ebrown55@terpmail.umd.edu>
2025-11-11 23:00:01 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -58,7 +58,7 @@ class Stepper:
         self.step_state += dir    # increment/decrement the step
         self.step_state %= 8      # ensure result stays in [0,7]
         with self.lock:
-            Stepper.shifter_outputs &= ~(0b1111<<self.shifter_bit_start)
+            Stepper.shifter_outputs.value &= ~(0b1111<<self.shifter_bit_start)
             Stepper.shifter_outputs |= Stepper.seq[self.step_state]<<self.shifter_bit_start
             self.s.shiftByte(Stepper.shifter_outputs.value)
         self.angle += dir/Stepper.steps_per_degree
