ac823c33afd0179696313eb85ab98b6e63f0641c
edwinsrbrown <ebrown55@terpmail.umd.edu>
2025-11-19 15:21:15 -0500

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -125,9 +125,11 @@ if __name__ == '__main__':
 
     
     m1.goAngle(-90)
+    """
     m1.goAngle(45)
     m1.goAngle(-90)
     m1.goAngle(45)
+    """
     """
     m2.goAngle(180)
     m2.goAngle(-45)
