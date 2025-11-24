b7690e7f30d5afaa85b316847504b8de024d7cb6
cchang04 <cchang04@terpmail.umd.edu>
2025-11-24 17:07:43 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,1 +1,71 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import RPi.GPIO as GPIO
+
+#Parse Function
+def parsePOSTdata(data):
+    data_dict = {}
+    idx = data.find('\r\n\r\n')+4
+    data = data[idx:]
+    data_pairs = data.split('&')
+    for pair in data_pairs:
+        key_val = pair.split('=')
+        if len(key_val) == 2:
+            data_dict[key_val[0]] = key_val[1]
+    return data_dict
+
+POWER = False
+PORT = 8080
+
+POWER_PIN = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(POWER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+class PowerHandler(BaseHTTPRequestHandler):
+  
+  def _generate_html(self):
+    global POWER
+    global POWER_PIN
+    POWER=GPIO.input(POWER_PIN)
+
+    if POWER = True:
+      status_text = "ON"
+      button_label = "Turn Off"
+
+    else:
+      status_text = "OFF"
+      button_label = "Turn On"
+
+    html_content = f"""
+
+<!DOCTYPE html>
 <html>
+<head>
+  <title>Power Control</title>
+</head>
+<body>
+  <h1>Device Status: <b>{status_text}</b></h1> 
+  <hr>
+
+  <form action="/" method="POST">
+    <button name="toggle_action" value="1">{button_label}</button>
+  </form>
+</body>
+</html>
+"""
+    return html_content
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+    self.wfile.write(self._generate_html().encade("utf-8"))
+
+  def do_POST(self):
+    content_length = int(self.headers['Content-Length'])
+    post_data_raw = self.rfile.read(content_length).decode('utf-8')
+
+    self.send_response(303)
+    self.send_header('Location', '/')
+    self.end_headers()
+
+def run_server():
+  server_address = ('', PORT)
