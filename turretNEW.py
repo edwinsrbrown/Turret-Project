77aa7568dee7c9de56701e8c6d9450e57ff35d46
cchang04 <cchang04@terpmail.umd.edu>
2025-12-01 16:57:22 -0500

    Update turretNEW.py

--- turretNEW.py
+++ turretNEW.py
@@ -218,7 +218,6 @@ class TurretHandler(BaseHTTPRequestHandler):
                 url = data.get("json_url","")
                 team = str(int(data.get("team_id","1")))
                 print(team)
-                # Still requires urllib.request for the GET
                 with urllib.request.urlopen(url) as r:
                     j = json.loads(r.read().decode('utf-8'))
 
